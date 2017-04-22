ce4b5574c67a0c9564993a7b327c6c6b0919dc1c
m7grey <m7grey@gmail.com>
2017-04-22 14:51:05 -0400

    add script php from 02.ReadFile.php

--- 01.CustomerReport.php
+++ 01.CustomerReport.php
@@ -1,4 +1,28 @@
+<!--01.CustomerReport.php-->
 <!DOCTYPE html>
+
+<?php
+$file = fopen("/data/10000.FIT.purchase.csv", "r");
+$line = fgets($file);
+$counter = 0;
+
+$upperLimit = 100;
+if (isset($_GET["limit"])){
+    $upperLimit = $_GET["limit"];
+}
+
+while (!feof($file) && $counter <= $upperLimit) {
+    $row = fgets($file);
+    $columns = explode(",", $row);
+    echo ($counter + 1) . ") " . $columns[0] . " - " . $columns[1]. " - " .$columns[2] . " - " . $columns[3] . $columns[4] . " - " . $columns[5];
+
+    echo "<hr>";
+    $counter++;
+}
+//close resources
+fclose($file);
+?>
+
 <!-- saved from url=(0031)file:///C:/bootstrap/table.html -->
 <html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title>Bootstrap Example</title>
