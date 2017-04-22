980499d35a4cc289dd27d52451492025a22786c7
m7grey <m7grey@gmail.com>
2017-04-22 13:35:10 -0400

    while loop <100

--- 01.ReadFile.php
+++ 01.ReadFile.php
@@ -1,12 +1,17 @@
 <!-- Created by PhpStorm. filename: 01.ReadFile.php-->
+<!-- explode http://php.net/manual/en/function.explode.php-->
 <?php
 $file = fopen("/data/10000.FIT.purchase.csv", "r");
 $line = fgets($file);
-while (!feof($file)) {
-    $row = fgets($file);
 
-    echo $row;
+$counter = 0;
+while ($counter < 100) {
+    $row = fgets($file);
+    $columns = explode(",", $row);
+    echo ($counter + 1) . ") " . $columns[0] . " - " . $columns[1]. " - " .$columns[2] . " - " . $columns[3] . $columns[4] . " - " . $columns[5];
+
     echo "<hr>";
+    $counter++;
 }
 //close resourses
 fclose($file);
