8742849dc7ecee92aa142b857e97d2d0ff2fdb94
m7grey <m7grey@gmail.com>
2017-04-22 14:27:30 -0400

    make it dynamic. 50 , 75, 88 ,

--- 02.ReadFile.php
+++ 02.ReadFile.php
@@ -1,11 +1,23 @@
 <!-- Created by PhpStorm. filename: 01.ReadFile.php-->
-<!-- explode http://php.net/manual/en/function.explode.php-->
+<!--<!-- explode http://php.net/manual/en/function.explode.php-->-->
+<!--Example -->
+<!--02.RF.php display 100 rows-->
+<!--50-->
+<!--75-->
+<!--88-->
+
+
 <?php
 $file = fopen("/data/10000.FIT.purchase.csv", "r");
 $line = fgets($file);
-
 $counter = 0;
-while (!feof($file) && $counter < 100) {
+
+$upperLimit = 100;
+if (isset($_GET["limit"])){
+    $upperLimit = $_GET["limit"];
+}
+
+while (!feof($file) && $counter < $upperLimit) {
     $row = fgets($file);
     $columns = explode(",", $row);
     echo ($counter + 1) . ") " . $columns[0] . " - " . $columns[1]. " - " .$columns[2] . " - " . $columns[3] . $columns[4] . " - " . $columns[5];
