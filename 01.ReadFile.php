e8ffba3e3db2ca791f82f51c9ffc55f4feb777d6
m7grey <m7grey@gmail.com>
2017-04-22 13:54:32 -0400

    better - work with any 100 10000

--- 01.ReadFile.php
+++ 01.ReadFile.php
@@ -5,7 +5,7 @@ $file = fopen("/data/10000.FIT.purchase.csv", "r");
 $line = fgets($file);
 
 $counter = 0;
-while ($counter < 100) {
+while (!feof($file) && $counter < 100) {
     $row = fgets($file);
     $columns = explode(",", $row);
     echo ($counter + 1) . ") " . $columns[0] . " - " . $columns[1]. " - " .$columns[2] . " - " . $columns[3] . $columns[4] . " - " . $columns[5];
